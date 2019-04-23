2b50b313d1f26f44d268689fa1e266e54f71bd6e
Heather Cleland <heathercleland@gmail.com>
2019-04-23 14:36:59 -0500

    finally have correct URL for pulling city-specific cuisine type

--- scripts/restaurants.js
+++ scripts/restaurants.js
@@ -11,8 +11,8 @@
 //         })
     
     
-function restaurantData(foodType) {
-fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138`, {
+// function restaurantData(foodType) {
+fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=308&start=first&sort=rating`, {
         headers: {
             "Accept": "application/json",
             "user-key": "01f4ed4db4b7be953e0d8582af28907d"
@@ -24,11 +24,20 @@ fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138`, {
         // const restaurantName = results.restaurants[3].restaurant.name;
         // const restaurantAddress = results.restaurants[3].restaurant.location.address;
         // const foodCuisine = results.restaurants[3].restaurant.cuisines;
+        // for (let i = 0; i < restaurantData.restaurants.length; i++) {
         console.log(restaurantData);
+        // }
     })
-}
+// }
 
-restaurantData("thai");
+// restaurantData("thai");
+
+// const selectElement = document.querySelector('.cuisineList');
+
+// selectElement.addEventListener('change', (event) => {
+//     const result = document.querySelector('.results-container');
+//     result.textContent = `You chose ${event.target.value}`;
+// });
 
 
 
